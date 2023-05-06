3617993d8c5ca4377b2b491af9ae4df1b60e25e2
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-05-07 00:18:22 +0300

    work with pagination

---
+++ src/js/components/pagination.js
@@ -0,0 +1,29 @@
+import Pagination from 'tui-pagination';
+const paginContainer = document.getElementById('tui-pagination-container');
+const pagOptions = {
+  itemsPerPage: 10,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+const pagInstance = new Pagination(paginContainer, pagOptions);
+// console.log(paginContainer);
+// console.log(pagInstance);
