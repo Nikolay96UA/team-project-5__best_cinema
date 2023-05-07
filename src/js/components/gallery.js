6dd07e08ae8dce5707c55a1a2f7f44fdb584821a
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-05-07 23:33:40 +0300

    added pagination, icons, clean code

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -1,47 +1,51 @@
 import { BASE_URL, API_KEY, URL_TREND_WEEK, URL_GENRE_LIST } from '../constants/api';
 import axios from 'axios';
+import { pagInstance } from './pagination';
 
 let currentPage = 1;
 let genresListArray = [];
 let idsArray = [];
 let categorysArray = [];
-let tenPaginationArr = [];
-let twentyPaginationArr = [];
 const galleryEl = document.getElementById('gallery');
 
 getMoviesWeekAndGenreList();
 async function getMoviesWeekAndGenreList() {
-  const { data: moviesObject } = await axios.get(
-    `${BASE_URL}${URL_TREND_WEEK}?api_key=${API_KEY}&page=${currentPage}&total_results=10000`
-  );
-  for (let i = 0; i < moviesObject.results.length; i += 1) {
-    if (tenPaginationArr.length < 10) {
-      tenPaginationArr.push(moviesObject.results[i]);
-    } else {
-      twentyPaginationArr.push(moviesObject.results[i]);
+  try {
+    const { data: moviesObject } = await axios.get(
+      `${BASE_URL}${URL_TREND_WEEK}?api_key=${API_KEY}&page=${currentPage}`
+    );
+
+    // console.log('tenPaginationArr', tenPaginationArr);
+    // console.log('twentyPaginationArr', twentyPaginationArr);
+    const { data: genresObject } = await axios.get(
+      `${BASE_URL}${URL_GENRE_LIST}?api_key=${API_KEY}`
+    );
+    console.log('result object', moviesObject);
+    genresListArray = genresObject.genres;
+    for (let i = 0; i < genresListArray.length; i += 1) {
+      idsArray.push(genresListArray[i].id);
+      categorysArray.push(genresListArray[i].name);
     }
+    // console.log('idsArray', idsArray);
+    // console.log('categorysArray', categorysArray);
+    // console.log('genresListArray', genresListArray);
+    createMarkUp(moviesObject.results);
+  } catch (error) {
+    console.log(error);
   }
-  console.log('tenPaginationArr', tenPaginationArr);
-  console.log('twentyPaginationArr', twentyPaginationArr);
-  const { data: genresObject } = await axios.get(`${BASE_URL}${URL_GENRE_LIST}?api_key=${API_KEY}`);
-  console.log('result object', moviesObject);
-  genresListArray = genresObject.genres;
-  for (let i = 0; i < genresListArray.length; i += 1) {
-    idsArray.push(genresListArray[i].id);
-    categorysArray.push(genresListArray[i].name);
-  }
-  // console.log('idsArray', idsArray);
-  // console.log('categorysArray', categorysArray);
-  // console.log('genresListArray', genresListArray);
-  createMarkUp(tenPaginationArr);
 }
 
 function createMarkUp(array) {
   const markup = array
-    .map(({ title, genre_ids, release_date, poster_path, backdrop_path }) => {
+    .map(({ title, genre_ids, release_date, poster_path, vote_average }) => {
       return `<li class="gallery-item" style="background-image: url(https://image.tmdb.org/t/p/w500${poster_path})"><div class="gallery-item__about"><h3 class="gallery-item__about__title">${title}</h3><p class="gallery-item__about__p">${getGenreForCard(
         genre_ids
-      )} | ${release_date.slice(0, 4)}</p></li>`;
+      )} | ${release_date.slice(
+        0,
+        4
+      )}</p></div><input class="input-vote" type="range" name="vote" min="0" max="10.0" step="0.1" value=${vote_average.toFixed(
+        1
+      )}></li>`;
     })
     .join('');
   renderMarkup(markup);
@@ -69,3 +73,13 @@ function getGenreForCard(genreIds) {
 }
 // <div class="for-img"></div>
 // style="background-image: url(https://image.tmdb.org/t/p/w500${poster_path})"
+
+pagInstance.on('beforeMove', event => {
+  const { page: pagPage } = event;
+  console.log(pagPage);
+  if (pagPage === 2) {
+    pagInstance.movePageTo(3);
+    console.log(pagPage);
+  }
+});
+// pagInstance.on('afterMove', ({ page: pagPage }) => console.log(pagPage));
--- src/js/components/pagination.js
+++ src/js/components/pagination.js
@@ -1,22 +1,25 @@
 import Pagination from 'tui-pagination';
 const paginContainer = document.getElementById('tui-pagination-container');
+const title = 'myTitle';
 const pagOptions = {
+  totalItems: 240,
   itemsPerPage: 10,
-  visiblePages: 5,
+  visiblePages: 3,
   page: 1,
   centerAlign: false,
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
+
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    page: '<a href="#" class="tui-page-btn page">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected curr-page">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}"></span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}"></span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
@@ -25,5 +28,4 @@ const pagOptions = {
   },
 };
 const pagInstance = new Pagination(paginContainer, pagOptions);
-// console.log(paginContainer);
-// console.log(pagInstance);
+export { pagInstance };
