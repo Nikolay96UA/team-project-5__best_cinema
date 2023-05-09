39f0619e2f106d9012cd362af9a6bf3804c180a8
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-05-09 20:29:39 +0300

    Update modal-pop-up.js

--- src/js/components/modals/modal-pop-up.js
+++ src/js/components/modals/modal-pop-up.js
@@ -1,9 +1,7 @@
-import { MovieDatabaseAPI } from '../../utils/fetchMovieDetails';
-import { onGalleryLinkClick } from '../gallery';
+import { MovieDatabaseAPI } from '../utils/fetchMovieDetails';
+// import { onGalleryLinkClick } from '../components/gallery';
+import { galleryEl } from '../components/gallery';
 
-const inputFilmID = document.querySelector('.film-id');
-const fetchDetailInfoBTN = document.querySelector('.fetch-detail-info');
-const form = document.querySelector('.form');
 const closeModalBtn = document.querySelector('[data-close-modal]');
 const backdrop = document.querySelector('[data-backdrop]');
 const modal = document.querySelector('.modal');
@@ -12,19 +10,19 @@ const container = document.querySelector('.wrap');
 const movieDatabaseAPI = new MovieDatabaseAPI();
 let detailMarkup;
 
-form.addEventListener('submit', fetchDetailInfo);
-closeModalBtn.addEventListener('click', toggleModal);
+// form.addEventListener('submit', fetchDetailInfo);
+// closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
   backdrop.classList.toggle('backdrop--hidden');
 }
 
-async function fetchDetailInfo(e) {
+async function fetchDetailInfo(movieId) {
   try {
-    e.preventDefault();
-    movieDatabaseAPI.query = e.currentTarget.elements.searchQuery.value;
-    console.log(movieDatabaseAPI.query);
-    const result = await movieDatabaseAPI.fetchMovieDetails();
+    // e.preventDefault();
+    // movieDatabaseAPI.query = e.currentTarget.elements.searchQuery.value;
+    // console.log(movieDatabaseAPI.query);
+    const result = await movieDatabaseAPI.fetchMovieDetails(movieId);
     // const secondResult = await fetchGenreDetails(result.genres.id);
     renderDetailMarkup(result);
     console.log(result);
@@ -80,26 +78,44 @@ function renderDetailMarkup({
         : ''
     }</div>
     <div class='container-content-wrap'>
-        <h3 class='title'>${original_title}</h3>
-        <div class='content-wrap'>
-            <div class='features-wrap'>
-                <p class='feature-name'>Vote / Votes</p>
-                <p class='feature-name'>Popularity</p>
-                <p class='feature-name'>Genre</p>
-            </div>
-            <div class='features-values-wrap'>
-                <p class='feature-value'>${vote_average}<span> / </span>${vote_count}</p>
-                <p class='feature-value'>${popularity}</p>
-                <p class='feature-value'>${Object.values(genres)
-                  .map(genre => genre.name)
-                  .join(' ')}</p>
-            </div>
+      <h3 class='title'>${original_title}</h3>
+      <div class='content-wrap'>
+        <div class='features-wrap'>
+          <p class='feature-name'>Vote / Votes</p>
+          <p class='feature-name'>Popularity</p>
+          <p class='feature-name'>Genre</p>
         </div>
-        <p class='feature-name feature-name-uppercase'>About</p>
-        <p class='feature-value feature-value-description'>${overview}</p>
-        <button class="add-to-library" type="button">Add to my library</button>
+        <div class='features-values-wrap'>
+          <p class='feature-value'>${vote_average}<span> / </span>${vote_count}</p>
+          <p class='feature-value'>${popularity}</p>
+          <p class='feature-value'>${Object.values(genres)
+            .map(genre => genre.name)
+            .join(' ')}</p>
+        </div>
+      </div>
+      <p class='feature-name feature-name-uppercase'>About</p>
+      <p class='feature-value feature-value-description'>${overview}</p>
+      <button class="add-to-library" type="button">Add to my library</button>
     </div>
-
+    <button class="close-modal" type="button" data-close-modal>
+      <svg width="24px" height="24px">
+        <use
+          class="close-modal__icon-close"
+          href="./images/icons/symbol-defs.svg#close-button"
+        ></use>
+      </svg>
+    </button>
     `;
   container.innerHTML = detailMarkup;
 }
+
+galleryEl.addEventListener('click', onGalleryLinkClick);
+
+function onGalleryLinkClick(event) {
+  if (event.target.nodeName === 'LI') {
+    const movieId = event.target.dataset.id;
+    fetchDetailInfo(movieId);
+  }
+}
+
+export { fetchDetailInfo };
